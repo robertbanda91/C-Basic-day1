3763a4f8087ab357339f8c9388da75e17f887d60
robertbanda91 <robertsonbanda91@gmail.com>
2025-10-01 00:44:04 +0000

    Update C-Basic-day1

---
+++ project7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    int largest = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+    printf("The largest number is %d\n", largest);
+
+    return 0;
+}
